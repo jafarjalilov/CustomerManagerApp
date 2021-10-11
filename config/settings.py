0cb08af9c7f95c7c2beb4a3ef10c5985ce987aa6
Jalilov Jafar <65230803+jafarjalilov@users.noreply.github.com>
2021-10-11 21:49:57 +0500

    Starter urls

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Own created apps
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home Page')
+
+def about(request):
+    return HttpResponse('About Page')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('about/', about, name='about')
 ]
