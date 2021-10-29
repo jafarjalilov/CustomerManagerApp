2026fd2199aec652e4c71d222c5401fa98e21f9c
Jalilov Jafar <65230803+jafarjalilov@users.noreply.github.com>
2021-10-29 16:55:28 +0500

    added basic urls

--- config/urls.py
+++ config/urls.py
@@ -14,19 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Home Page')
-
-def about(request):
-    return HttpResponse('About Page')
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('about/', about, name='about')
+    path('', include('accounts.urls')),
 ]
